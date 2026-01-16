dc7f68b73d97d3fdef9ce91b85c6a3f6bafd8ffc
UXDev25 <unai.alminana.7e9@itb.cat>
2026-01-16 08:24:22 +0100

    Added the configuration of the base project

--- app/src/main/java/com/example/trivialapp_base/MainActivity.kt
+++ app/src/main/java/com/example/trivialapp_base/MainActivity.kt
@@ -11,13 +11,32 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.trivialapp_base.ui.theme.TrivialAPP_BaseTheme
+import com.example.trivialapp_base.view.GameScreen
+import com.example.trivialapp_base.view.MenuScreen
+import com.example.trivialapp_base.view.ResultScreen
+import com.example.trivialapp_base.view.SplashScreen
+import com.example.trivialapp_base.viewmodel.GameViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val navigationController = rememberNavController()
+            NavHost(
+                navController = navigationController,
+                startDestination = Routes.ScrSplash.route
+            ) {
+                /*composable(Routes.ScrMainMenu.route) { GameScreen(navigationController)}
+                composable(Routes.ScrGame.route) { MenuScreen(navigationController) }
+                composable(Routes.ScrResult.route) { ResultScreen(navigationController) }*/
+                composable(Routes.ScrSplash.route) { SplashScreen(navigationController) }
+            }
             TrivialAPP_BaseTheme {
                 // Controlador de navegación
 
--- app/src/main/java/com/example/trivialapp_base/Routes.kt
+++ app/src/main/java/com/example/trivialapp_base/Routes.kt
@@ -1,5 +1,8 @@
 package com.example.trivialapp_base
 
 sealed class Routes(val route: String) {
-
+    object ScrMainMenu:Routes("mainmenu")
+    object ScrGame:Routes("game")
+    object ScrResult:Routes("result")
+    object ScrSplash:Routes("splash")
 }

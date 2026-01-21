a3c082fddb8005e47e10e2bd797e925b7fa4d9c0
UXDev25 <unai.alminana.7e9@itb.cat>
2026-01-21 09:50:59 +0100

    feat: added game branch from Unai Almiñana

--- app/src/main/java/com/example/trivialapp_base/view/Screen_game.kt
+++ app/src/main/java/com/example/trivialapp_base/view/Screen_game.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -17,17 +19,27 @@ import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.navigation.NavController
 import com.example.trivialapp_base.R
+import com.example.trivialapp_base.Routes
 import com.example.trivialapp_base.viewmodel.GameViewModel
 
 @Composable
 fun GameScreen(navController: NavController, viewModel: GameViewModel) {
-    var optionSelected by remember { mutableStateOf("No selected") }
-
     ConstraintLayout(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.Black)
     ) {
-
+        val (buttonOne, buttonTwo, buttonThree, buttonFour, timeQuestion, edgyQuestion) = createRefs()
+        Button(
+            onClick = {
+            },
+            modifier = Modifier.constrainAs(buttonOne) {
+                top.linkTo(edgyQuestion.bottom, margin = 20.dp) // Debajo del log
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            }
+        ) {
+            Text(text = "Play")
+        }
     }
 }

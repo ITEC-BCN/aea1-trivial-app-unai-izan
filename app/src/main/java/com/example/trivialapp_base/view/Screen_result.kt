919773ba843fcf5c8370665d6fd437bf4d0219f7
Izan <izan.gonzalez.7e8@itb.cat>
2026-01-29 10:07:19 +0100

    Feature: Doing Screen_result.kt buttons dissany.

--- app/src/main/java/com/example/trivialapp_base/view/Screen_result.kt
+++ app/src/main/java/com/example/trivialapp_base/view/Screen_result.kt
@@ -1,16 +1,25 @@
 package com.example.trivialapp_base.view
 
+import android.R.attr.end
+import android.R.attr.start
+import android.R.attr.top
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -21,6 +30,9 @@ import androidx.navigation.NavController
 import com.example.trivialapp_base.R
 import com.example.trivialapp_base.Routes
 import com.example.trivialapp_base.viewmodel.GameViewModel
+import kotlinx.coroutines.NonDisposableHandle
+import kotlinx.coroutines.NonDisposableHandle.parent
+
 @Composable
 fun ResultScreen(navController: NavController, viewModel: GameViewModel) {
     ConstraintLayout(
@@ -43,7 +55,6 @@ fun ResultScreen(navController: NavController, viewModel: GameViewModel) {
                     end.linkTo(parent.end)
                 }
         )
-
         Card(modifier = Modifier.constrainAs(scoreRef) {
             top.linkTo(titleRef.bottom, margin = 20.dp) // Debajo del score
             start.linkTo(parent.start)
@@ -58,32 +69,57 @@ fun ResultScreen(navController: NavController, viewModel: GameViewModel) {
         }
 
         //Share button
+        Box(
+            modifier = Modifier.constrainAs(shareBtn) {
+                top.linkTo(scoreRef.bottom, margin = 120.dp) // Debajo del score
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            }
+                .height(150.dp)
+                .width(200.dp)
+                .background(colorResource(id = R.color.Ferrari_Red))
+        ) { }
         Button(
             onClick = {
             },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Transparent
+            ),
             modifier = Modifier.constrainAs(shareBtn) {
                 top.linkTo(scoreRef.bottom, margin = 120.dp) // Debajo del score
                 start.linkTo(parent.start)
                 end.linkTo(parent.end)
             }
         ) {
+
             Text(text = "Share",
                 fontFamily = FontFamily(Font(R.font.fugazone)))
         }
 
-        Button(
-            onClick = {
-                viewModel.iniciarJuego()
-                navController.navigate(Routes.ScrMainMenu.route)
-            },
+        Box(
             modifier = Modifier.constrainAs(menuBtn) {
-                top.linkTo(scoreRef.bottom, margin = 200.dp)
-                start.linkTo(parent.start)
-                end.linkTo(parent.end)
+                top.linkTo(shareBtn.bottom, margin = 20.dp)
+                start.linkTo(parent.start, margin = 150.dp)
             }
+                .height(150.dp)
+                .width(200.dp)
+                .background(colorResource(id = R.color.Ferrari_Red))
         ) {
-            Text(text = "Return to menu",
-                fontFamily = FontFamily(Font(R.font.fugazone)))
+            Button(
+                onClick = {
+                    viewModel.iniciarJuego()
+                    navController.navigate(Routes.ScrMainMenu.route)
+
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Transparent)
+            ) {
+                Text(
+                    text = "Return to menu",
+                    fontFamily = FontFamily(Font(R.font.fugazone))
+                )
+            }
         }
+
     }
 }
